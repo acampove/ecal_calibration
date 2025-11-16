937aa29da759910ef238ae8dba4c66807f24435c
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-16 12:33:25 +0800

    Add Momenta to namespace

--- src/ecal_calibration/__init__.py
+++ src/ecal_calibration/__init__.py
@@ -1,4 +1,5 @@
 from .objective  import Objective
 from .jpsi_decay import JpsiDecay
+from .jpsi_decay import Momenta 
 
-__all__ = ['Objective', 'JpsiDecay']
+__all__ = ['Objective', 'JpsiDecay', 'Momenta']
