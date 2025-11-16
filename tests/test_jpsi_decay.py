9f08ddb787e534feb856b5e9418e74bc862f2c3c
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-16 18:09:16 +0800

    Update to check plots

--- tests/test_jpsi_decay.py
+++ tests/test_jpsi_decay.py
@@ -3,16 +3,59 @@ Module meant to test JpsiDecay class and its dependent, Momenta
 '''
 import numpy
 import vector
+import pandas            as pnd
+import matplotlib.pyplot as plt
 
+from vector           import MomentumObject4D as Momentum
 from ecal_calibration import JpsiDecay
 from ecal_calibration import Momenta 
 
+# ----------------------
+def _vector_from_df(name : str, df : pnd.DataFrame) -> list[Momentum]:
+    '''
+    Parameters
+    -------------
+    name: Particle name, needed to pick column in dataframe
+    df  : Dataframe with kinematics
+
+    Returns
+    -------------
+    List of 4-vectors
+    '''
+    arr_px = df[f'{name}_px'].to_numpy()
+    arr_py = df[f'{name}_py'].to_numpy()
+    arr_pz = df[f'{name}_pz'].to_numpy()
+    arr_en = df[f'{name}_e' ].to_numpy()
+
+    l_particle = []
+    for px, py, pz, en in zip(arr_px, arr_py, arr_pz, arr_en):
+        particle = vector.obj(px=px, py=py, pz=pz, e=en)
+        l_particle.append(particle)
+
+    return l_particle
+# ----------------------
+def _plot_mass(df : pnd.DataFrame) -> None:
+    '''
+    Parameters
+    -------------
+    df : Pandas dataframe with kinematics
+    '''
+    l_e1 = _vector_from_df(name='e1', df=df)
+    l_e2 = _vector_from_df(name='e2', df=df)
+
+    l_mass = []
+    for e1, e2 in zip(l_e1, l_e2):
+        jpsi : Momentum = e1 + e2 # type: ignore
+        l_mass.append(jpsi.m)
+
+    plt.hist(l_mass, bins=100)
+    plt.show()
 # ----------------------
 def test_simple_jpsi_decay() -> None:
     '''
     Simplest test
     '''
-    nentries = 100
+    nentries = 10000
 
     obj = JpsiDecay(nentries = nentries)
     df  = obj.get_dataframe()
@@ -20,12 +63,14 @@ def test_simple_jpsi_decay() -> None:
     columns = set(df.columns.to_list())
     assert columns == {
         'g1_co', 'g1_rw', 'g1_ar',
-        'gm_px', 'gm_py', 'gm_pz',
-        'e1_px', 'e1_py', 'e1_pz',
-        'e2_px', 'e2_py', 'e2_pz',
+        'gm_px', 'gm_py', 'gm_pz', 'gm_e',
+        'e1_px', 'e1_py', 'e1_pz', 'e1_e',
+        'e2_px', 'e2_py', 'e2_pz', 'e2_e',
     } 
 
     assert len(df) == nentries 
+
+    _plot_mass(df=df)
 # ----------------------
 def test_simple_momenta() -> None:
     '''
