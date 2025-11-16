f9532167b6c992fed11d7bd1c3010920f3cebc1f
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-16 12:33:42 +0800

    Adapt test to test Momenta

--- tests/test_jpsi_decay.py
+++ tests/test_jpsi_decay.py
@@ -1,14 +1,26 @@
 '''
-Module meant to test JpsiDecay class
+Module meant to test JpsiDecay class and its dependent, Momenta
 '''
+import tensorflow as tf
 
-from rx_calibration import JpsiDecay
+from ecal_calibration import JpsiDecay
+from ecal_calibration import Momenta 
 
 # ----------------------
-def test_simple() -> None:
+def test_simple_jpsi_decay() -> None:
     '''
     Simplest test
     '''
     obj = JpsiDecay()
     df  = obj.get_dataframe()
-    
+# ----------------------
+def test_simple_momenta() -> None:
+    '''
+    Simplest test for Momenta class
+    '''
+    particles = tf.ones(shape=(2, 4))
+
+    obj = Momenta(particles=particles)
+    px  = obj['px']
+    py  = obj['py']
+    pz  = obj['pz']
