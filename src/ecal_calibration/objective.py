fd0e5753e182d4154fde98a75285a377ff1120e2
Angel Campoverde <angel.campoverde@cern.ch>
2025-11-16 18:57:45 +0800

    Add basic prototyping

--- src/ecal_calibration/objective.py
+++ src/ecal_calibration/objective.py
@@ -2,6 +2,7 @@
 Module with the Objective class
 '''
 import pandas as pnd
+import numpy
 import jax
 
 # -------------------------------
@@ -24,6 +25,32 @@ class Objective:
         df : Pandas dataframe
         '''
         self._df = df
+
+    # ----------------------
+    def _get_masses(self, x : jax.Array) -> numpy.ndarray:
+        '''
+        Parameters
+        -------------
+        x: Array with scale factors
+
+        Returns
+        -------------
+        Array with masses
+        '''
+
+        return numpy.random.normal(size=100)
+    # ----------------------
+    def _get_fom(self, masses : numpy.ndarray) -> float:
+        '''
+        Parameters
+        -------------
+        masses: Array with Jpsi masses
+
+        Returns
+        -------------
+        Figure of merit, used to quantify quality of calibration
+        '''
+        return masses.std()
     # ----------------------
     def __call__(self, x : jax.Array) -> float:
         '''
@@ -35,5 +62,8 @@ class Objective:
         -------------
         Value of loss function, e.g. objective
         '''
-        return 1.0 
+        masses = self._get_masses(x=x)
+        fom    = self._get_fom(masses=masses)
+
+        return fom 
 # -------------------------------
